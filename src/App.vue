03ae778c515c1b3c354bd03ca6c53ee31e11088f
ethon92 <e19920526@gmail.com>
2022-04-23 15:42:37 +0800

    create alpha pos system project and complete order bill part ui

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import OrderBill from './components/OrderBill.vue';
+</script>
+
+<template>
+  <main class="container">
+    <h2 class="text-center text-light pt-4">Alpha Pos System</h2>
+    <div class="row">
+      <div class="col-md-4 p-4">
+        <OrderBill />
+        <div class="text-end">
+          <button class="btn btn-light" style="min-width: 120px;">Checkout</button>
+        </div>
+      </div>
+      <div class="col-md-8">yaa</div>
+    </div>  
+  </main>
+</template>
+
+<style>
+  .container{
+    background-color: rgba(0, 0, 0, 0.8);
+    border-radius: 3px;
+  }
+
+  .col-md-4, .col-md-8 {
+    outline: 1px solid steelblue;
+    min-height: 600px;
+  }
+</style>
---
+++ src/components/OrderBill.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card mb-3">
+    <div class="card-body pt-3 pr-3">
+      <div class="text-end">
+        <span class="delete-drink">×</span>
+      </div>
+      <h6 class="card-title mb-1">Black Tea</h6>
+      <div class="card-text">Less Ice</div>
+      <div class="card-text">Half Sugar</div>
+    </div>
+      
+    <div class="card-footer text-end">
+      <div class="card-text text-muted py-2">
+        $ <span>30</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .delete-drink {
+    display: inline-block;
+    text-align: center;
+    color: #999;
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    font-size: 20px;
+    line-height: 1;
+  }
+
+  .delete-drink:hover {
+    color: #333;
+  }
+</style>
